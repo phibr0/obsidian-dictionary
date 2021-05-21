31bd99ea6ae948bb4acdbd0aca57dbee76c495cf
Phillip <bronzel.phillip+github@gmail.com>
2021-05-21 14:30:26 +0200

    Add help text

--- src/apiManager.ts
+++ src/apiManager.ts
@@ -3,8 +3,21 @@ import type { DictionarySettings } from "src/settings";
 
 import FreeDictionaryAPI from "src/api/freeDictionaryAPI"
 
+/*
+
+HOW TO ADD A NEW API:
+
+1. Add a new class that implements DefinitionProvider or
+SynonymProvider (or both) and put the file in /src/api/
+2. Push the new Provider to the right list in the
+APIManager, as seen below
+3. Test the Solution
+4. Create a new Pull Request on GitHub
+
+*/
+
 export default class APIManager {
-    settings: DictionarySettings
+    private settings: DictionarySettings
 
     definitionProvider: DefinitionProvider[] = [];
     synonymProvider: SynonymProvider[] = [];
@@ -19,10 +32,22 @@ export default class APIManager {
         )
     }
 
+    /**
+     * Sends a request with the passed query to the chosen API and returns the Result
+     *
+     * @param query - The term you want to look up
+     * @returns The API Response of the chosen API as Promise<DictionaryWord>
+     */
     async requestDefinitions(query: string): Promise<DictionaryWord> {
         return await this.getDefinitionAPI().requestDefinitions(query);
     }
 
+    /**
+     * Sends a request with the passed query to the chosen API and returns the resulting Synonyms
+     *
+     * @param query - The term you want to look up
+     * @returns The API Response of the chosen API as Promise<string[]>
+     */
     async requestSynonyms(query: string): Promise<string[]> {
         return await this.getSynonymAPI().requestSynonyms(query);
     }

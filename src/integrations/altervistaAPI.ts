efdedbabde659b4e61a11e9a82e47ce9a00cea06
phibr0 <bronzel.phillip@gmail.com>
2021-07-24 20:23:45 +0200

    migrate Altervista API

--- src/integrations/altervistaAPI.ts
+++ src/integrations/altervistaAPI.ts
@@ -1,3 +1,4 @@
+import { request } from "obsidian";
 import type{ PartOfSpeech, Synonym, SynonymProvider } from "src/integrations/types";
 
 export class AltervistaSynonymProvider implements SynonymProvider {
@@ -21,25 +22,18 @@ export class AltervistaSynonymProvider implements SynonymProvider {
     //This is limited to 5000 queries/day
     TOKEN = "P4QAmqYIN1DY6XjlQJht"
 
-    //Yes this doesnt use https
-    //We need a Proxy to add cors headers
-    API_END_POINT = "https://api.allorigins.win/get?url=" + encodeURIComponent('http://thesaurus.altervista.org/thesaurus/v1');
-
     async requestSynonyms(query: string, lang: string, _?: PartOfSpeech): Promise<Synonym[]> {
         const synonyms: Synonym[] = [];
-        let result: Response;
+        let result: string;
         try {
-            result = await fetch(this.constructRequest(query, lang));
+            result = await request({url: this.constructRequest(query, lang)});
         } catch (error) {
             return Promise.reject(error);
         }
-        if (result.status != 200) {
-            return Promise.reject();
-        }
 
-        const json = await result.json();
+        const json = await JSON.parse(result);
 
-        for(const c of JSON.parse(json.contents).response){
+        for(const c of json.response){
             const words = c.list.synonyms.split('|');
             words.forEach((word: string) => {
                 synonyms.push({word: word});
@@ -50,7 +44,7 @@ export class AltervistaSynonymProvider implements SynonymProvider {
     }
 
     constructRequest(query: string, lang: string): string {
-        return this.API_END_POINT + encodeURIComponent(`?word=${query}&key=${this.TOKEN}&language=${this.languageCodes[lang]}&output=json`);
+        return `http://thesaurus.altervista.org/thesaurus/v1?word=${query}&key=${this.TOKEN}&language=${this.languageCodes[lang]}&output=json`;
     }
 
 }
